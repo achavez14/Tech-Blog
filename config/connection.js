535a3faeb438bc815602bee046b2e329d0bbf605
Alexa Chavez <aguinada14@gmail.com>
2024-04-10 22:10:16 -0700

    connect to postgresql

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,11 @@
-let sequelize;
+const { Sequelize } = require('sequelize');
 
-if (process.env.DB_URL) {
-  sequelize = new Sequelize(process.env.DB_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PW,
-    {
-      host: 'localhost',
-      dialect: 'postgres',
-    },
-  );
-}
+const sequelize = new Sequelize(
+  process.env.DB_URL || {
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PW,
+    host: 'localhost',
+    dialect: 'postgres'
+  }
+);
