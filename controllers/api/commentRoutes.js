cc71ce931156873e47eed9644a37566646a38ca4
Alexa Chavez <aguinada14@gmail.com>
2024-04-10 18:57:10 -0700

    Added code to commentRoutes.js file in controllers directory of api folder to implement addComment and deleteComment fucntions

---
+++ controllers/api/commentRoutes.js
@@ -0,0 +1,32 @@
+const Comment = require('../models/Comment');
+
+const commentController = {
+    addComment: async (req, res) => {
+        const { content, author, postId } = req.body;
+        try {
+            // Add new comment to blog post
+            const newComment = await Comment.create({ content, author, postId });
+            return res.status(201).json({ message: 'Comment added successfully', comment: newComment });
+        } catch (error) {
+            console.error(error);
+            return res.status(500).json({ message: 'Server error' });
+        }
+    },
+
+    deleteComment: async (req, res) => {
+        const { commentId } = req.params;
+        try {
+            // Find the comment by ID and delete it
+            const deletedComment = await Comment.findByIdAndDelete(commentId);
+            if (!deletedComment) {
+                return res.status(404).json({ message: 'Comment not found' });
+            }
+            return res.status(200).json({ message: 'Comment deleted successfully' });
+        } catch (error) {
+            console.error(error);
+            return res.status(500).json({ message: 'Server error' });
+        }
+    }
+};
+
+module.exports = commentController;
