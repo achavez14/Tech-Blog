83fb1b610e8e61c124cd7d6c88b08502c5f44bf6
Alexa Chavez <aguinada14@gmail.com>
2024-04-10 18:47:27 -0700

    Added code to blogpostRoutes.js file in controllers directory of api folder to implement createPost function

---
+++ controllers/api/blogpostRoutes.js
@@ -0,0 +1,25 @@
+const BlogPost = require('../models/BlogPost');
+
+const blogPostController = {
+    createPost: async (req, res) => {
+        const { title, content, author } = req.body;
+        try {
+            // Create a new blog post
+            const newPost = await BlogPost.create({ title, content, author });
+            return res.status(201).json({ message: 'Blog post created successfully', post: newPost });
+        } catch (error) {
+            console.error(error);
+            return res.status(500).json({ message: 'Server error' });
+        }
+    },
+
+    updatePost: async (req, res) => {
+        // Logic to update an existing blog post
+    },
+
+    deletePost: async (req, res) => {
+        // Logic to delete a blog post
+    }
+};
+
+module.exports = blogPostController;
