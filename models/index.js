f4e7ef423e54e93fc0f3ee193a1ce86a293b9138
Alexa Chavez <aguinada14@gmail.com>
2024-04-10 19:40:20 -0700

    Added code to index.js file in models folder to import individual model files and export them as an object

---
+++ models/index.js
@@ -0,0 +1,11 @@
+// Import all model files
+const Comment = require('./Comment');
+const BlogPost = require('./BlogPost');
+const User = require('./User');
+
+// Export all models
+module.exports = {
+    Comment,
+    BlogPost,
+    User
+};
